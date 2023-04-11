70b9295f6394aed9bc57bc0a2e892dcfaf461e93
s990093 <128201642+s990093@users.noreply.github.com>
2023-04-11 23:28:52 +0800

    cancel sql

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,24 +83,24 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# 外網
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_test',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': '49.213.238.75',
-        'PORT': '3306', # MySQL數據庫的端口號，通常是3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# 外網
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django_test',
+#         'USER': 'admin',
+#         'PASSWORD': 'admin',
+#         'HOST': '49.213.238.75',
+#         'PORT': '3306', # MySQL數據庫的端口號，通常是3306
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
