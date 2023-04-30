a68bf60ebc28debcd89ff8d2c6759c5316d58142
fainstar <37238989+fainstar@users.noreply.github.com>
2023-04-30 21:33:34 +0800

    123

--- login/urls.py
+++ login/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', views.sign_up, name = 'register'),
     path('login/', views.sign_in, name = 'login'),
     path('logout/', views.log_out, name = 'logout'),
-    path('auth/', include('social_django.urls', namespace='social')),
+    #path('auth/', include('social_django.urls', namespace='social')),
     path('test/', views.testuser, name = 'testuser'),
 ]
