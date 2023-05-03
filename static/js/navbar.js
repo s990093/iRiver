9b1cde2497ee5b1ace0dc379bab56aa2386f113b
s990093 <128201642+s990093@users.noreply.github.com>
2023-05-03 16:16:59 +0800

    1

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,15 +1,9 @@
 $('.login').on('click', function () {
-    console.log($('.login'))
-
     fetch('/user/isLogin')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            if (data.isLogin) {
-                console.log(data);
-            } else {
+            if (!data.isLogin)
                 location.href = '/user/login/';
-            }
         });
 });
 
