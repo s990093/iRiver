330b0f2da57d4a66ee80fe92ca7ff2539b527dd7
fainstar <37238989+fainstar@users.noreply.github.com>
2023-05-05 16:48:19 +0800

    錯誤訊息2

--- login/models.py
+++ login/models.py
@@ -5,6 +5,7 @@ from django import forms
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxLengthValidator
 
 # Create your models here.
 COUNTRY_CHOICES = (
@@ -15,6 +16,16 @@ COUNTRY_CHOICES = (
     ('TW', 'Taiwan'),
 )
 
+#電話格式檢查
+phone_test = RegexValidator(
+    regex=r'^\+?\d{9,15}$',
+    message="電話號碼錯誤、應在9～15個數字之間 ＋號可有可無"
+)
+user_test = RegexValidator(
+    regex=r'^.{1,10}$',
+    message="使用者名稱錯誤、應在1～10個字之間"
+)
+#生日格式檢查
 def validate_birthday(value):
     min_date = datetime.date(1900, 1, 1)
     max_date = datetime.date.today()
@@ -23,9 +34,10 @@ def validate_birthday(value):
             _('Invalid birthday - must be between %(min_date)s and today'),
             params={'value': value, 'min_date': min_date},
         )
+    
 class UserProfile(models.Model):
-    user = models.CharField(max_length=100)
+    user = models.CharField(max_length=100, validators=[user_test])
     mail = models.EmailField(max_length=100, primary_key=True)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20,validators=[phone_test])
     country = models.CharField(max_length=2, choices=COUNTRY_CHOICES)
     birthday = models.DateField(null=True, blank=True, validators=[validate_birthday])
