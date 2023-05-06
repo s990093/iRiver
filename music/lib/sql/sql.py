a50445a475d4ceb559a1a07beb8d7104e2ff11bb
fainstar <37238989+fainstar@users.noreply.github.com>
2023-05-06 22:01:11 +0800

    Update sql.py

--- music/lib/sql/sql.py
+++ music/lib/sql/sql.py
@@ -183,7 +183,8 @@ class SQL:
                 break
         return result, score
     
-    def get_music_list_info(self, muic_ID_list):
-    # def get_song_url(self , song_url):
+    #def get_music_list_info(self, muic_ID_list):
+        # def get_song_url(self , song_url):
+   
     def close(self):
         self.db.close()
