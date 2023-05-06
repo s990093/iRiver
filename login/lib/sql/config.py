8d29ff02c23118568fc25b27fe721d27de3a26a1
fainstar <37238989+fainstar@users.noreply.github.com>
2023-05-06 14:42:03 +0800

    123

---
+++ login/lib/sql/config.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    'host': '49.213.238.75',
+    'port': 3306,
+    'user': 'gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr',
+    'password': 'JABmQsQhpj05F6WI',
+    'db': 'iriver_user',
+    'charset': 'utf8mb4',
+}
+
