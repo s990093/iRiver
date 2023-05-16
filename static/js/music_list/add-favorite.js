18bfc9f423f05806d8649702d92aa98e7978b1d8
s990093 <128201642+s990093@users.noreply.github.com>
2023-05-16 13:45:05 +0800

    123123122

--- static/js/music_list/add-favorite.js
+++ static/js/music_list/add-favorite.js
@@ -31,50 +31,50 @@ export class FaController {
 
     _listener() {
         const self = this;
+        // 加入個人專輯
         $(".add").on("click", function () {
             $("#favoriteModal").modal("show");
+            self.insert_song_infos = { "music_ID": $(this).attr('value'), "favorite": false };
+            console.log(self.insert_song_infos);
             self.showFa()
         });
 
-        // 加入個人專輯
-        $(".add").on("click", function () {
-            var music_ID = $(this).attr('value');
-            var favorite = false;
-            this.insert_song_infos = { "music_ID": music_ID, "favorite": favorite };
-            console.log(this.insert_song_infos)
-        });
 
         // 我的最愛
-        $(".love-iocn").on("click", function () {
+        $('#table-body').on('click', '.love-icon a', async function () {
             $(this).find('i').toggleClass('far fas');
             var music_ID = $(this).attr('value');
             // 送出
             insert_my_music_list({
                 music_ID: music_ID,
-                music_list: "favorite",
+                music_list: "我的最愛",
                 favorite: true
             });
         });
-
-
     }
 
     _get_playlist() {
         return this.fetch.get_playlist();
     }
 
-    playlist_template(playlist) {
+    playlist_template(playlist, isChecked = false) {
         const row = `
         <div class="row">
             <div class="col">
                 <label class="playlist mb-3" data-playlist="${playlist}">
-                    <input type="radio" name="playlist" id="${playlist}" autocomplete="off">
+                    <input 
+                    type="radio" 
+                    name="playlist" 
+                    id="${playlist}"
+                    autocomplete="off" 
+                    ${isChecked ? "checked" : ""}
+                    >
                     ${playlist}
                 </label>
             </div>
         </div>
-
         `;
+
         return row;
     }
 
@@ -84,29 +84,32 @@ export class FaController {
      */
     showFa = () => {
         const self = this;
-        // console.log('show fa model', this.playlist);
+        $(".fa-body").html("");
+        $(".fa-body").html(self.playlist_template("我的最愛", true));
         $('.fa-body').html(this.playlist.map(function (playlist) {
             return self.playlist_template(playlist);
         }));
 
+
         // 添加到哪個專輯
-        $(".playlist").on("click", function () {
-            console.log(self.insert_song_infos);
+        $(".fa-body").on("click", ".playlist", function () {
             self.insert_song_infos = {
                 "playlist": $(this).data('playlist'),
                 "music_ID": self.insert_song_infos.music_ID,
                 "favorite": self.insert_song_infos.favorite
             }
-            console.log(self.insert_song_infos);
-
+            // console.log(self.insert_song_infos);
         });
 
         // 送出
-        $("#fa-insert").on("click", async function () {
+        $(".fa-body").on("click", ".fa-insert", async function () {
             const success = await insert_my_music_list(self.insert_song_infos);
             console.log(success);
         });
     }
 
-    createFa = () => { }
+    createFa = (playlist) => {
+        this.playlist = [...playlist];
+        $(".fa-body").on("click", () => { this.playlist_template4(playlist); });
+    }
 }
