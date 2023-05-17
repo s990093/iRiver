2581c9223a0440c00e7921ad6f7576a6b8b05af0
s990093 <128201642+s990093@users.noreply.github.com>
2023-05-18 02:54:11 +0800

    21312123

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -1,6 +1,11 @@
+import { Fetch } from "./fetch.js";
+import { error } from "./error.js";
+
 export class PlaylistController {
     constructor() {
-        this.playlists = []
+        this.fetch = new Fetch();
+        this.playlists = [];
+        this.target = "/user/get_user_music_list/";
     }
 
     push_playlist = (playlists) => {
@@ -17,14 +22,36 @@ export class PlaylistController {
             self._showPL();
             $("#playlistModal").modal("show");
         });
+
+        // delete
+        $("#playlistModal .modal-body .playlist").on("click", ".delete", async function () {
+            var playlist = $(this).data("playlist");
+            const params = { method: "delete_playlist", playlist: playlist };
+            const success = await self.fetch.POST(self.target, params);
+            if (success) {
+                $(`#${playlist}`).removes();
+            }
+        });
+
+        // edit
+        $("#playlistModal .modal-body").on("click", ".edit", async function () {
+            location.href = `/music/my_music_list?music_list=${$(this).data("playlist")}`;
+        });
+
+        // add
+        $("#playlistModal .modal-body").on("click", ".add", async function () {
+            var playlist = $(this).data("playlist");
+            const params = { method: "delete_playlist", playlist: playlist };
+            const response = await self.fetch.POST(self.target, params);
+        });
+
     }
 
     _playlist_template(playlist) {
-        console.log(playlist);
         return `
-        <div class="row">
+        <div class="row playlist" id= "${playlist}">
             <div class="col-6">
-            <label class="playlist mb-3" data-playlist="${playlist}">
+            <label class="mb-3" data-playlist="${playlist}">
                 ${playlist}
             </label>
             </div>
@@ -33,7 +60,7 @@ export class PlaylistController {
                     <a href="javascript:void(0)" class="delete mr-2">
                         <i class="bi bi-trash-fill"></i>
                     </a>
-                    <a href="javascript:void(0)" class="delete">
+                    <a href="javascript:void(0)" class="edit">
                         <i class="fa-solid fa-hammer"></i>
                     </a>
                     <a href="javascript:void(0)" class="add">
