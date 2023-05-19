d12620e8534ecd3e29f2e9e012e4bf9797c6d583
s990093 <128201642+s990093@users.noreply.github.com>
2023-05-20 01:35:09 +0800

    111111

--- iRiver/static/js/user_setting.js
+++ iRiver/static/js/user_setting.js
@@ -1,19 +1,46 @@
 import { Fetch } from "../../../static/js/fetch.js";
+import { SessionController } from "./../../../static/js/session.js";
 import { error } from "../../../static/js/error.js";
 
+/**
+ * Create a new instance of User_setting.
+ * @constructor
+ * @param {string} name - The name of the User_setting.
+ * @param {data} user_setting  - The user setting object data 
+
+ */
 
 class User_setting {
-    constructor(name) {
+    constructor() {
+        // 物件宣告
         this.fetch = new Fetch();
+        this.sessionController = new SessionController();
+
         this._register()
+        this.traget = "/user/get_user_session/";
     }
 
     _register = () => {
-        
+        const user_setting = this.sessionController.get('user_setting');
+        if (user_setting !== undefined && user_setting !== null) {
+            this._show();
+        }
+
+        const params = { get: "user_setting" };
+        const response = this.fetch.POST(this.traget, params);
+        if (response.status === 200) {
+            sessionStorage.setItem("user_setting", response.data.user_setting);
+            this._show();
+        }
         this._listener();
     }
 
     _listener = () => { }
+
+    _show = () => {
+
+
+    }
 }
 
 
--- static/js/session.js
+++ static/js/session.js
@@ -1,4 +1,74 @@
+/**
+ * SessionController class for managing session data using sessionStorage.
+ */
 export class SessionController {
-    constructor(options) { }
-    
+    /**
+     * Create a new instance of SessionController.
+     * @constructor
+     * @param {Object} options - Optional configuration options.
+     */
+    constructor(options = null) {
+        // Retrieve session data from sessionStorage
+        let session = JSON.parse(sessionStorage.getItem("session"));
+
+        // Initialize session if it's undefined or null
+        if (session === undefined || session === null) {
+            session = [];
+            sessionStorage.setItem("session", JSON.stringify(session));
+        }
+    }
+
+    /**
+     * Add an item to the session.
+     * @param {any} item - The item to be added to the session.
+     */
+    add = (item) => {
+        let session = JSON.parse(sessionStorage.getItem("session"));
+
+        // Add item to the session array
+        session.push(item);
+
+        // Update session data in sessionStorage
+        sessionStorage.setItem("session", JSON.stringify(session));
+
+        // Store item separately in sessionStorage using its value as the key
+        sessionStorage.setItem(item, JSON.stringify(item));
+    }
+
+    /**
+     * Get the value of a specific item in the session.
+     * @param {string} item - The key of the item to retrieve.
+     * @returns {any} The value of the requested item.
+     */
+    get = (item) => {
+        // Retrieve and parse the item from sessionStorage
+        return JSON.parse(sessionStorage.getItem(item));
+    }
+
+    /**
+     * Show all items in the session.
+     */
+    show = () => {
+        let session = JSON.parse(sessionStorage.getItem("session"));
+
+        // Display each item in the session array
+        session.map((item) => {
+            console.log(item);
+        });
+    }
+
+    /**
+     * Clear the session by removing all items.
+     */
+    clear = () => {
+        let session = JSON.parse(sessionStorage.getItem("session"));
+
+        // Remove each item from sessionStorage
+        session.forEach((item) => {
+            sessionStorage.removeItem(item);
+        });
+
+        // Clear the session array from sessionStorage
+        sessionStorage.removeItem("session");
+    }
 }
